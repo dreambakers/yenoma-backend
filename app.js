3ef488c07a79e1a337060dcfd78c8f2795c40897
mhd3v <mamir3012@gmail.com>
2020-06-23 20:00:47 +0500

    Cater for SSL certificate

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const https = require('https');
 const cors = require('cors');
+const fs = require('fs');
 
 const routes = require('./routes/routes');
 
@@ -8,12 +10,19 @@ const app = express();
 app.use(cors({origin:true,credentials: true})); // allow cors headers
 app.use(bodyParser.json());
 
+const key = fs.readFileSync(__dirname + '/certs/privkey.key');
+const cert = fs.readFileSync(__dirname + '/certs/certificate.crt');
+const options = {
+  key: key,
+  cert: cert
+};
+
 const port = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'x-auth,x-filesize,Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'x-auth,Content-Type');
     res.setHeader('Access-Control-Expose-Headers', 'x-auth,Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
@@ -23,7 +32,9 @@ app.get('/', (req, res) => { res.send('Hello world!') });
 
 app.use('/', routes);
 
-app.listen(port, () => {
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
