6f812fb73cdf4c57ac2b45c288fa523640e75f2a
mhd3v <mamir3012@gmail.com>
2020-07-27 19:50:57 +0500

    Update subcription on successful payment

--- api/payment/payment.controller.js
+++ api/payment/payment.controller.js
@@ -1,27 +1,30 @@
 const { Payment } = require('./payment.model');
+const { User } = require('../user/user.model');
 const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
 const payPalClient = require('../utility/paypal-client');
 const constants = require('../../constants');
+const moment = require('moment');
 
 const capturePayment = async (req, res) => {
     try {
-        const { orderId } = req.body;
-        console.log(orderId)
-
+        const { orderId, subscriptionPeriod } = req.body;
         const request = new checkoutNodeJssdk.orders.OrdersCaptureRequest(orderId);
         request.requestBody({});
 
         const capture = await payPalClient.client().execute(request);
 
-        if (capture.result === 'COMPLETED') {
+        if (capture.result.status === 'COMPLETED') {
+            req.user.subscription.expires = moment(req.user.subscription.expires).add(subscriptionPeriod.duration, 'days').toISOString();
+            await req.user.save();
             const newPayment = new Payment({
                 result: capture.result,
                 for: req.user._id
             });
-
             await newPayment.save();
-
-            return res.send(200);
+            return res.json({
+                success: 1,
+                user: req.user
+            });
         }
     } catch (error) {
         console.log(error);
