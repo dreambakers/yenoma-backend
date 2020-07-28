bfa7cf55d6accc7cc04d5a0a8fb01c46ee7456ad
mhd3v <mamir3012@gmail.com>
2020-07-28 17:36:58 +0500

    Implement price verification for approved purchase of subscription

--- api/payment/payment.controller.js
+++ api/payment/payment.controller.js
@@ -1,5 +1,4 @@
 const { Payment } = require('./payment.model');
-const { User } = require('../user/user.model');
 const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
 const payPalClient = require('../utility/paypal-client');
 const constants = require('../../constants');
@@ -8,26 +7,31 @@ const moment = require('moment');
 const capturePayment = async (req, res) => {
     try {
         const { orderId, subscriptionPeriod } = req.body;
-        const request = new checkoutNodeJssdk.orders.OrdersCaptureRequest(orderId);
-        request.requestBody({});
-
-        const capture = await payPalClient.client().execute(request);
-
-        if (capture.result.status === 'COMPLETED') {
-            const user = req.user;
-            const baseDate = new Date() < new Date(user.subscription.expires) ? user.subscription.expires : new Date();
-            user.subscription.expires = moment(baseDate).add(subscriptionPeriod.duration, 'days').toISOString();
-            await user.save();
-            const newPayment = new Payment({
-                result: capture.result,
-                for: user._id
-            });
-            await newPayment.save();
-            return res.json({
-                success: 1,
-                user
-            });
+        let request = new checkoutNodeJssdk.orders.OrdersGetRequest(orderId);
+        let order = await payPalClient.client().execute(request);
+        const selectedSubscriptionPeriod = constants.subscriptionPeriods.find(subscription => subscription.duration === subscriptionPeriod.duration);
+        if (selectedSubscriptionPeriod.price && order.result.purchase_units[0].amount.value == selectedSubscriptionPeriod.price) {
+            request = new checkoutNodeJssdk.orders.OrdersCaptureRequest(orderId);
+            request.requestBody({});
+            const capture = await payPalClient.client().execute(request);
+            if (capture.result.status === 'COMPLETED') {
+                const user = req.user;
+                const baseDate = new Date() < new Date(user.subscription.expires) ? user.subscription.expires : new Date();
+                user.subscription.expires = moment(baseDate).add(subscriptionPeriod.duration, 'days').toISOString();
+                await user.save();
+                const newPayment = new Payment({
+                    result: capture.result,
+                    for: user._id
+                });
+                await newPayment.save();
+                return res.json({
+                    success: 1,
+                    user
+                });
+            }
+            throw { captureFailed: true, capture };
         }
+        throw { priceVerificationFailed: true };
     } catch (error) {
         console.log(error);
         res.json({
