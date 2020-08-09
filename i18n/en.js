bb567b5583af218b41f6e06a945d2b48cedd9ae9
mhd3v <mamir3012@gmail.com>
2020-08-09 19:18:14 +0500

    Update email confirmation template

--- i18n/en.js
+++ i18n/en.js
@@ -6,7 +6,7 @@ module.exports = {
         thankYouForUsing: "Thank you for using yenoma",
         youCanChangePassword: "You can change your password by clicking on the link below within 48 hours.",
         feedbackReceivedFromYenomaUser: "feedback received from yenoma user",
-        emailConfirmation: "Please confirm that {{email}} is your email address by clicking on the link below within 48 hours",
+        emailConfirmation: "Please confirm your email address by clicking on the link below within 48 hours.",
         previewText: "Text goes here"
     },
 
